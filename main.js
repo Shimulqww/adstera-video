6f9514cee1ff54dc104983a619ee817760585470
Shimulqww <shimulmiah074@gmail.com>
2025-05-30 13:28:48 +0600

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-<script>
 const adLinks = [
   "https://www.profitableratecpm.com/kf3qprjcu?key=f4646d0b196b3e6ba057b2f620d35491",
   "https://www.profitableratecpm.com/khfhbjjr?key=8f6a2c9025532f3798bca037c9f902d3",
@@ -9,30 +8,25 @@ const adLinks = [
   "https://www.profitableratecpm.com/kixcddqy?key=58dffefd81444669a11b2e92dc2343b7",
   "https://www.profitableratecpm.com/wespy8absx?key=084b70eb5e1c03a2e5a571bb1ec60f4e",
   "https://www.profitableratecpm.com/u4i1yq64?key=42d086a08caedd4e6874ebd095d6d930",
-  "https://www.profitableratecpm.com/hxvc24wcr2?key=cafac74a2cc1288ccef07cd14b67485e",
-  "https://www.profitableratecpm.com/f10b8qhnf?key=ffc13e308f2488c273996f5d758e64de",
-  "https://www.profitableratecpm.com/j04ei85dtx?key=c77b132a71e2b86a6f2e40140f5790c8",
-  "https://www.profitableratecpm.com/mj0zcu0aq6?key=9a1f1b5248d8070c12b6632b2ebfaae4",
-  "https://www.profitableratecpm.com/yiz66v847?key=395c8e2b8c70e34f0f7b37bf9dd005d4",
-  "https://www.profitableratecpm.com/katmrgyd?key=823822718d29c4f59470ee65e2e00378",
-  "https://www.profitableratecpm.com/wdvk7bbga?key=c95a371cb2ab6d366a405451f9ca7e7b",
-  "https://www.profitableratecpm.com/kgwkw0njd?key=c11b8830a028cc574bde2494d5956ae9",
-  "https://www.profitableratecpm.com/f9m79cm6?key=79a149ef6aab089aec946c6e85ca1c0a",
-  "https://www.profitableratecpm.com/rxvsyw1625?key=ab0125f4a626e7f1b062882968c352ee"
+  "https://www.profitableratecpm.com/hxvc24wcr2?key=cafac74a2cc1288ccef07cd14b67485e"
 ];
 
 function launchAds() {
-  const randomIndex = Math.floor(Math.random() * adLinks.length);
-  const adUrl = adLinks[randomIndex];
-  const adWindow = window.open(adUrl, '_blank');
+  // Pick a random ad link
+  const adLink = adLinks[Math.floor(Math.random() * adLinks.length)];
 
-  // Give a short delay for popup, then redirect main window
+  // Trap back button to redirect to your site
+  history.pushState(null, null, location.href);
+  window.onpopstate = function () {
+    location.href = "https://adstera-7v8.pages.dev/";
+  };
+
+  // Open the ad in a new tab
+  const w = window.open(adLink, '_blank');
+  if (w) w.blur();
+
+  // Redirect main tab after 1 second
   setTimeout(() => {
-    window.location.href = "https://www.profitableratecpm.com/j4dphx0z?key=15cbaf51f773cf8bb9df2cd3bc048ec5";
-  }, 500); // 500ms delay
+    window.location.href = "https://adstera-7v8.pages.dev/";
+  }, 1000);
 }
-</script>
-
-<!-- Button to start -->
-<button onclick="launchAds()">Continue to Video</button>
-
